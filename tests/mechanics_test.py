3bb883a9803efd33481f871639b87c6785853b0e
shimon-d <sdegli@proton.me>
2023-03-16 09:34:03 -0300

    changed the classes into functions for better usage and design

--- tests/mechanics_test.py
+++ tests/mechanics_test.py
@@ -1,11 +1,4 @@
-from functvs import Subfactorial
-from functvs import Pythagoras
-from functvs import Factorial
-from functvs import Euclid
+from functvs import double_factorial, subfactorial
 
-
-print(Subfactorial.euler(5))
-print(Pythagoras.leg_hypotenuse(a=10.15, b=59.33, c=60.3))
-print(Pythagoras.perimeter(10, 10, 10))
-print(Euclid.isosceles_area(base=30, height=40))
-print(Factorial.factorial(5))
+print(double_factorial(5))
+print(subfactorial(5))
